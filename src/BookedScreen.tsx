f7eaf43b6ae17a831e32884ac994996b75fd0d5a
unknown <0r20@mail.ru>
2020-11-25 20:00:24 +0300

    Booked screen

--- src/BookedScreen.tsx
+++ src/BookedScreen.tsx
@@ -1,18 +1,53 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { Post } from "./components/Post";
+import { DATA } from "./data";
+import { IPost } from "./../types/post";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { AppHeaderIcon } from "./components/AppHeaderIcon";
 
-export const BookedScreen = () => {
+export const BookedScreen = ({ navigation }: { navigation: any }) => {
+  const openPostHandler = (post: IPost) => {
+    navigation.navigate("Post", {
+      postId: post.id,
+      date: post.date,
+      booked: post.booked,
+    });
+  };
+
   return (
     <View style={styles.center}>
-      <Text>BookedScreen</Text>
+      <FlatList
+        data={DATA.filter((post) => post.booked)}
+        keyExtractor={(post) => post.id}
+        renderItem={({ item }) => {
+          return <Post post={item} onOpen={openPostHandler} />;
+        }}
+      />
     </View>
   );
+};
+
+BookedScreen.navigationOptions = () => {
+  return {
+    headerTitle: "Избранное",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item
+          title="Drower"
+          iconName="ios-menu"
+          onPress={() => console.log("Press drower")}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
   center: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
   },
 });
--- src/navigations/AppNavigations.tsx
+++ src/navigations/AppNavigations.tsx
@@ -1,9 +1,13 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { MainScreen } from "../MainScreen";
 import { PostScreen } from "../PostScreen";
 import { THEME } from "../theme";
+import React from "react";
+import { BookedScreen } from "../BookedScreen";
 
 const PostNavigator = createStackNavigator(
   {
@@ -23,4 +27,46 @@ const PostNavigator = createStackNavigator(
   }
 );
 
-export const AppNavigation = createAppContainer(PostNavigator);
+const BookedNavigator = createStackNavigator(
+  {
+    Booked: BookedScreen,
+    Post: PostScreen,
+  },
+  {
+    initialRouteKey: "Booked",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "#fff",
+      },
+      headerTintColor: Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR,
+    },
+  }
+);
+
+const BottomNavigator = createBottomTabNavigator(
+  {
+    Post: {
+      screen: PostNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+    Booked: {
+      screen: BookedNavigator,
+      navigationOptions: {
+        tabBarIcon: (info) => (
+          <Ionicons name="ios-star" size={25} color={info.tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: THEME.MAIN_COLOR,
+    },
+  }
+);
+
+export const AppNavigation = createAppContainer(BottomNavigator);
